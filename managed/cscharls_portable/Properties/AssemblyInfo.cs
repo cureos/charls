40089e671d788875eee7ef767418eaffc430c204
Anders Gustafsson <anders@cureos.com>
2017-04-13 17:08:00 +0200

    Strong naming.

--- managed/cscharls_portable/Properties/AssemblyInfo.cs
+++ managed/cscharls_portable/Properties/AssemblyInfo.cs
@@ -10,4 +10,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("cscharls")]
 [assembly: AssemblyDescription("")]
 
-[assembly: InternalsVisibleTo("cscharls_tests")]
+[assembly: InternalsVisibleTo("cscharls_tests, PublicKey=" +
+    "002400000480000094000000060200000024000052534131000400000100010015b9558a3d86b7" +
+    "6370af6ffa68b97a0c9a4cc0c25fea9fbb3df9a3c4fc432125bd92aaaa8d22f036ba0c08380a2d" +
+    "ce7273e7b95c22f6d97629b9fa27efdfc8393d594f731969499669f39a2f335501acd56f420002" +
+    "6c4925f474e8b50ad7eafe0fa5e235dababad076e09a796489bd3d6432ef1c76d0851992de77a0" +
+    "fd3970b4")]
