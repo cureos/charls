cd85d6520b054d3d3d4c28ec9559f176fa49d68d
Anders Gustafsson <anders@cureos.com>
2017-05-04 19:28:46 +0200

    Skip tests when files are not available.

--- managed/cscharls_tests/Performance.cs
+++ managed/cscharls_tests/Performance.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using Xunit;
+using Xunit.Sdk;
 
 using static CharLS.Util;
 
@@ -35,7 +36,7 @@ namespace CharLS
         }
 
         [Theory]
-        ////[InlineData("test/bad.raw", 0, 512, 512,  8, 1);
+        [InlineData("test/bad.raw", 0, 512, 512,  8, 1, false, 1, Skip = "File not available")]
         [InlineData("test/alphatest.raw", 0, 380, 287, 8, 4, false, 1)] // RGBA image (This is a common PNG sample)
         [InlineData("test/MR2_UNC", 1728, 1024, 1024, 16, 1, true, 1)] // 16 bit mono
         [InlineData("test/0015.raw", 0, 1024, 1024, 8, 1, false, 1)] // 8 bit mono
@@ -43,18 +44,18 @@ namespace CharLS
         [InlineData("test/desktop.ppm", 40, 1280, 1024, 8, 3, false, 1)] // 8 bit color
         [InlineData("test/SIEMENS-MR-RGB-16Bits.dcm", -1, 192, 256, 12, 3, true, 1)] // 12 bit RGB
         [InlineData("test/DSC_5455.raw", 142949, 300, 200, 16, 3, true, 1)] // 16 bit RGB
-        [InlineData("test/rgb8bit/artificial.ppm", 17, 3072, 2048, 8, 3, false, 1)] // 16 bit RGB
-        [InlineData("test/rgb8bit/bridge.ppm", 17, 2749, 4049, 8, 3, false, 1)] // 16 bit RGB
-        [InlineData("test/rgb8bit/flower_foveon.ppm", 17, 2268, 1512, 8, 3, false, 1)] // 16 bit RGB
-        ////[InlineData("test/rgb8bit/big_building.ppm", 17, 7216,5412,  8, 3, false, 1)]
-        ////[InlineData("test/rgb16bit/bridge.ppm", 19, 2749,4049,  16, 3, true, 1)]
+        [InlineData("test/rgb8bit/artificial.ppm", 17, 3072, 2048, 8, 3, false, 1, Skip = "File not available")] // 16 bit RGB
+        [InlineData("test/rgb8bit/bridge.ppm", 17, 2749, 4049, 8, 3, false, 1, Skip = "File not available")] // 16 bit RGB
+        [InlineData("test/rgb8bit/flower_foveon.ppm", 17, 2268, 1512, 8, 3, false, 1, Skip = "File not available")] // 16 bit RGB
+        [InlineData("test/rgb8bit/big_building.ppm", 17, 7216,5412,  8, 3, false, 1, Skip = "File not available")]
+        [InlineData("test/rgb16bit/bridge.ppm", 19, 2749,4049,  16, 3, true, 1, Skip = "File not available")]
         public void PerformanceTests(string strName, int ioffs, int width, int height, int cbit, int ccomp,
             bool littleEndianFile, int loopCount)
-        {
+        { 
             TestFile(strName, ioffs, new Size(width, height), cbit, ccomp, littleEndianFile, loopCount);
         }
 
-        [Theory]
+        [Theory(Skip = "File not available")]
         [InlineData(1)]
         public void DecodePerformanceTests(int loopCount)
         {
